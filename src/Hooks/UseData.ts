575d71b52f366a095a2da4179a4f44ed9eeae94b
[yeminhein95] <[yeminhei.tgo2@gmail.com]>
2024-01-24 09:43:46 +0630

    add baseurl and usedata hopks

---
+++ src/Hooks/UseData.ts
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { AxiosRequestConfig, CanceledError } from "axios";
+import BaseUrl from "../Services/BaseUrl";
+
+
+interface FetchReponse<T>{
+    count: number,
+    results : T[]
+}
+const useData =<T> (endpoint: string,requestConfig?: AxiosRequestConfig,deps?: any[])=>{
+    const [data, setGenres] = useState<T[]>([]);
+    const [error, setError] = useState("");
+    const [loding,Setloding] = useState(false);
+    useEffect(() => {
+        Setloding(true);
+        const controller = new AbortController;
+      BaseUrl
+        .get<FetchReponse<T>>(endpoint,{signal:controller.signal,...requestConfig})
+        .then((res) =>{ setGenres(res.data.results);
+        Setloding(false)})
+        .catch((err) => {
+             if(err instanceof CanceledError) return; 
+            setError(err.message)
+        Setloding(false)
+        });
+        
+        return()=> controller.abort();
+    },deps?[...deps]: []);
+
+    return {data,error,loding}
+}
+
+export default useData;
